37c81b45dae32093cec436f62e8b1044e6632ac5
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-22 14:59:36 -0700

    refactoring

--- examples/simple-bot.js
+++ examples/simple-bot.js
@@ -1,21 +1,18 @@
 const Telegraf = require('telegraf')
 const RedisSession = require('../lib/session')
 
-const telegraf = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-// Redis client available at session.client
-const session = new RedisSession()
+const session = new RedisSession({ttl: 5})
 
-telegraf.use(session.middleware())
+bot.use(session.middleware())
 
-telegraf.on('text', (ctx, next) => {
+bot.on('text', (ctx, next) => {
   ctx.session.counter = ctx.session.counter || 0
   ctx.session.counter++
   return next()
 })
 
-telegraf.hears('/stats', (ctx) => {
-  return ctx.reply(`${ctx.session.counter} messages from ${ctx.from.username}`)
-})
+bot.hears('/stats', ({ reply, session, from }) => reply(`${session.counter} messages from ${from.username}`))
 
-telegraf.startPolling(30)
+bot.startPolling()
