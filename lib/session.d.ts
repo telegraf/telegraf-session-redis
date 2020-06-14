6c5829ee129718255ab8841d887404f9a6c0de7a
Dmytro Filipenko <wind.fd@gmail.com>
2020-06-14 12:17:01 +0800

    Update session.d.ts

--- lib/session.d.ts
+++ lib/session.d.ts
@@ -1,32 +1,41 @@
 declare module 'telegraf-session-redis' {
     import { RedisClient } from 'redis';
     import { Context } from 'telegraf';
+  
+    export interface RetryStrategyOptions {
+        error: NodeJS.ErrnoException;
+        total_retry_time: number;
+        times_connected: number;
+        attempt: number;
+    }
 
+    export type RetryStrategy = (options: RetryStrategyOptions) => number | Error | undefined;
+  
     interface StoreOptions {
-      readonly host?: string;
-      readonly port?: number;
-      readonly path?: string;
-      readonly url?: string;
-      readonly parser?: string;
-      readonly string_numbers?: boolean;
-      readonly return_buffers?: boolean;
-      readonly detect_buffers?: boolean;
-      readonly socket_keepalive?: boolean;
-      readonly socket_initial_delay?: number;
-      readonly no_ready_check?: boolean;
-      readonly enable_offline_queue?: boolean;
-      readonly retry_max_delay?: number;
-      readonly connect_timeout?: number;
-      readonly max_attempts?: number;
-      readonly retry_unfulfilled_commands?: boolean;
-      readonly auth_pass?: string;
-      readonly password?: string;
-      readonly db?: string | number;
-      readonly family?: string;
-      readonly rename_commands?: { [command: string]: string } | null;
-      readonly tls?: any;
-      readonly prefix?: string;
-      readonly retry_strategy?: RetryStrategy;
+        readonly host?: string;
+        readonly port?: number;
+        readonly path?: string;
+        readonly url?: string;
+        readonly parser?: string;
+        readonly string_numbers?: boolean;
+        readonly return_buffers?: boolean;
+        readonly detect_buffers?: boolean;
+        readonly socket_keepalive?: boolean;
+        readonly socket_initial_delay?: number;
+        readonly no_ready_check?: boolean;
+        readonly enable_offline_queue?: boolean;
+        readonly retry_max_delay?: number;
+        readonly connect_timeout?: number;
+        readonly max_attempts?: number;
+        readonly retry_unfulfilled_commands?: boolean;
+        readonly auth_pass?: string;
+        readonly password?: string;
+        readonly db?: string | number;
+        readonly family?: string;
+        readonly rename_commands?: { [command: string]: string } | null;
+        readonly tls?: any;
+        readonly prefix?: string;
+        readonly retry_strategy?: RetryStrategy;
     }
 
     interface RedisOptions {
